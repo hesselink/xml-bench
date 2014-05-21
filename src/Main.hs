7203076575f128d08dd9e8a7b863f193ecd458c9
Erik Hesselink <hesselink@gmail.com>
2014-05-21 18:00:49 +0200

    Add parsing benchmark.

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,22 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE
+    OverloadedStrings
+  , StandaloneDeriving
+  , DeriveGeneric
+  #-}
 module Main where
 
 import Prelude hiding ((.), id)
 
 import Control.Category
 import Control.DeepSeq
+import Control.DeepSeq.Generics
 import Control.Monad
 import Criterion.Main
-import Text.XML.HXT.Core (runLA, getText, deep, isText, hasName, xread)
+import GHC.Generics
+import Text.XML.HXT.Core (runLA, getText, deep, isText, hasName, xread, XmlTrees)
 import Text.XML.Light
 import Text.XML.Light.Lexer (XmlSource)
-import Text.XML (parseText_, def)
+import Text.XML (parseText_, def, Document)
 import Text.XML.Cursor (fromDocument, descendant, orSelf, content, checkName)
 
 import qualified Data.Text         as Strict
@@ -25,7 +31,13 @@ main = do
   ltxt <- Lazy.readFile "data/collect-bold-text.xml"
   deepseq str $ deepseq txt $ deepseq ltxt
     defaultMain
-      [ bgroup "collect bold text"
+      [ bgroup "parse"
+        [ bench "hxt"                (nf hxtParse str)
+        , bench "xml (String)"       (nf xmlParse str)
+        , bench "xml (Text)"         (nf xmlParse txt)
+        , bench "xml-conduit (Text)" (nf xmlConduitParse ltxt)
+        ]
+      , bgroup "collect bold text"
         [ bench "hxt"                (nf hxtCollect str)
         , bench "xml (String)"       (nf xmlCollect str)
         , bench "xml (Text)"         (nf xmlCollect txt)
@@ -40,11 +52,31 @@ hxtCollect = runLA
   . xread
   )
 
+hxtParse :: String -> XmlTrees
+hxtParse = runLA xread
+
 xmlCollect :: XmlSource a => a -> [String]
 xmlCollect = map strContent
            . concatMap (findElements (QName "b" Nothing Nothing))
            . onlyElems
            . parseXML
+
+xmlParse :: XmlSource a => a -> [Content]
+xmlParse = parseXML
+
+deriving instance Generic Content
+deriving instance Generic CData
+deriving instance Generic Element
+deriving instance Generic CDataKind
+deriving instance Generic Attr
+deriving instance Generic QName
+
+instance NFData Content   where rnf = genericRnf
+instance NFData CData     where rnf = genericRnf
+instance NFData Element   where rnf = genericRnf
+instance NFData CDataKind where rnf = genericRnf
+instance NFData Attr      where rnf = genericRnf
+instance NFData QName     where rnf = genericRnf
 
 xmlConduitCollect :: Lazy.Text -> [Strict.Text]
 xmlConduitCollect =  content
@@ -53,3 +85,6 @@ xmlConduitCollect =  content
                  <=< orSelf descendant
                   .  fromDocument
                   .  parseText_ def
+
+xmlConduitParse :: Lazy.Text -> Document
+xmlConduitParse = parseText_ def
